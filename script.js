9356f417551e190328adaf17d298dc5352f048f7
Tahaberk SOYSAL <berksysl.g@gmail.com>
2024-01-02 19:54:42 +0300

    bug fixed

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 const answers = document.querySelectorAll('.answer');
 const imgElements = document.querySelectorAll('.question-container img');
 
+imgElements.forEach(img => {
+    img.addEventListener('keypress', (e) => {
+        if(e.key == 'Enter'){
+            img.click();
+        }
+    })
+})
+
 imgElements.forEach((el, index) => {
     el.addEventListener('click', () => toggleIcon(index));
 })
 
 function toggleIcon(i) {
     //change icons
-    if(imgElements[i].src == 'http://127.0.0.1:5500/assets/images/icon-minus.svg'){
-        imgElements[i].src = 'http://127.0.0.1:5500/assets/images/icon-plus.svg';
+    if(imgElements[i].attributes[0].value == './assets/images/icon-minus.svg'){
+        imgElements[i].attributes[0].value = './assets/images/icon-plus.svg';
     }else {
-        imgElements[i].src = 'http://127.0.0.1:5500/assets/images/icon-minus.svg';
+        imgElements[i].attributes[0].value = './assets/images/icon-minus.svg';
     }
 
     //hide or show answers
     answers[i].classList.toggle('hide');
-
 }
-
